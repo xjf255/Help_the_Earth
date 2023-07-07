eb76a53b1e0ac5ed5dbd2dd687b2328d694cb900
Fernando <111396436+xjf255@users.noreply.github.com>
2023-07-07 12:28:45 -0600

    feat(imgs) and docs(all)
    this is a websito to about help the Earth.

---
+++ README.txt
@@ -0,0 +1,1 @@
+this is a website to about help the earth
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-export default function () {
-    if(true){
-        console.log("hola")
-    }
-}
+const menuActive = document.querySelector('.btn')
+const nav = document.querySelector('.navigator_responsive-nav')
+const home = document.querySelector('.body')
+
+menuActive.addEventListener('click', () => {
+    home.classList.toggle('home-active')
+    nav.classList.toggle('navigator_responsive-nav--active')
+})
+nav.addEventListener('click', () => {
+    home.classList.toggle('home-active')
+    nav.classList.toggle('navigator_responsive-nav--active')
+})
+
+
+// menuActive.addEventListener('click', () => {
+//     console.warn('cagon')
+//     home.classList.toggle('home-active')
+//     menuActive.classList.toggle('navigator_responsive-nav--active')
+//     if (menuActive == true){
+//         home.addEventListener('click', () =>{
+//             home.classList.toggle('home-active')
+//             menuActive.classList.toggle('navigator_responsive-nav--active')
+//         })
+//     }
+//     console.log('Esa mierda tiene que salir')
+// })
+// console.log(menuActive)
